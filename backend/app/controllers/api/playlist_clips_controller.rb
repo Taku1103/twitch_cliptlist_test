2a1c20c5a6473d754d2cb12ef37954d2b8792fb8
Taku1103 <orzorz1243@gmail.com>
2024-01-31 00:11:59 +0900

    :sparkles: playlist_clipsの基本挙動を作成(APIテストで確認済)

--- backend/app/controllers/api/playlist_clips_controller.rb
+++ backend/app/controllers/api/playlist_clips_controller.rb
@@ -2,17 +2,27 @@ class Api::PlaylistClipsController < ApplicationController
   before_action :set_val_playlist_clip, only: [:create, :destroy]
 
   def create
+    # order_index処理追加予定
+    # PlaylistClip.all.length+1の数をorder_indexに加える
     @playlist_clips_relation = PlaylistClip.new(clip_id: @clip_id, playlist_id: @playlist_id)
+
     if @playlist_clips_relation.save
-      render json: { status: :created, message: "Sucessed adding the clip(#{clip_id}) in this playlist(#{playlist_id})" }
+      render json: { status: :created, message: "Sucessed adding the clip(clip_id:#{@clip_id}) in this playlist(playlist_id:#{@playlist_id})" }
     else
-      render json: { status: :unprocessable_entity, message: "Failed adding the clip(#{clip_id}) in this playlist(#{playlist_id})" }
+      render json: { status: :unprocessable_entity, message: "Failed adding the clip(#{@clip_id}) in this playlist(#{@playlist_id})" }
     end
   end
 
+  # 現状 req.bodyにclip_idとplaylist_idを作ってもらう
   def destroy
     @playlist_clips_relation = PlaylistClip.find_by(playlist_id: @playlist_id, clip_id: @clip_id)
     @playlist_clips_relation.destroy
+  end
+
+  # デバッグ用アクション(不要になったらコメントアウト)
+  def index
+    @playlist_clips_relations = PlaylistClip.all
+    render json: { status: :ok, message: "Sucessed Getting Index" , playlist_clips_relation: @playlist_clips_relations}
   end
 
   private
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     root 'home#index'
     resources :clips
+    resources :playlist_clips # ,only: [:create, :destroy]
     resources :users do
       scope module: :users do
         member do
