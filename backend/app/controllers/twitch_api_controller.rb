a87162fd255353e5e8a6be1480f53d5083b8cd7f
takuya-38 <takuya.kogoma.38@gmail.com>
2024-01-30 23:28:15 +0900

    :sparkles: データ取得用アカウントのフォローしているチャンネルのbroadcaster_id取得機能追加

--- backend/app/controllers/twitch_api_controller.rb
+++ backend/app/controllers/twitch_api_controller.rb
@@ -29,7 +29,7 @@ class TwitchApiController < ApplicationController
     render html: html_content.html_safe
   end
 
-  # アプリアクセストークンを使用して、データ取得用アカウントのidを取得
+  # アプリアクセストークンを使用して、rabibit君のidを取得
   def get_user
     header = { "Authorization" => ENV["APP_ACCESS_TOKEN"],  "Client-id" => ENV["CLIENT_ID"] }
     uri = "https://api.twitch.tv/helix/users?login=#{ENV["RABBIT_LOGIN_NAME"]}"
@@ -39,13 +39,24 @@ class TwitchApiController < ApplicationController
     render json: { "user_id": user_id }, status: :ok
   end
 
+  # rabibit君のフォローしているチャンネルのbroadcaster_idを取得（total: 183channels)
+  # チャンネル取得は最大100件/1リクエストのため、合計2回リクエストする。
+  # backend/public/broadcaster_idsにbroadcaster_id一覧を出力する。get_broadcaster_clipsで使用。
   def get_user_channels
-    header = { "Authorization" => ENV["APP_ACCESS_TOKEN"],  "Client-id" => ENV["CLIENT_ID"] }
-    uri = "https://api.twitch.tv/helix/channels/followed?broadcaster_id=#{login_name}"
+    header = { "Authorization" => ENV["USER_ACCESS_TOKEN"],  "Client-id" => ENV["CLIENT_ID"] }
 
-    res = request_get(header, uri)
-    user_id = res["data"][0]["id"]
-    render json: { "user_id": user_id }, status: :ok
+    uri = "https://api.twitch.tv/helix/channels/followed?user_id=#{ENV["RABBIT_ID"]}&first=100"
+    response = request_get(header, uri)
+    broadcaster_list = response["data"]
+    after = response["pagination"]["cursor"]
+
+    uri = "https://api.twitch.tv/helix/channels/followed?user_id=#{ENV["RABBIT_ID"]}&first=100&after=#{after}"
+    response = request_get(header, uri)
+    broadcaster_list.concat(response["data"])
+
+    broadcaster_ids = broadcaster_list.map { |broadcaster| broadcaster["broadcaster_id"].to_i }
+    File.write("public/broadcaster_ids.txt", broadcaster_ids.join("\n"))
+    render json: { "broadcaster_ids": broadcaster_ids }, status: :ok
   end
 
   private
