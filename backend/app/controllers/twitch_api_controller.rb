e6588a0878d293e6ae2c091c40aa9b8f1d5d83d5
takuya-38 <takuya.kogoma.38@gmail.com>
2024-01-30 18:37:52 +0900

    :sparkles: データ取得用アカウントのid取得機能の追加

--- backend/app/controllers/twitch_api_controller.rb
+++ backend/app/controllers/twitch_api_controller.rb
@@ -2,11 +2,8 @@ require "http"
 require "json"
 
 class TwitchApiController < ApplicationController
-  def show
-    render status: :ok
-  end
-
-  def create
+  # アプリアクセストークン取得
+  def create_app_token
     header = { "Content-Type" => "application/json" }
     uri = "https://id.twitch.tv/oauth2/token"
     body = {
@@ -15,9 +12,30 @@ class TwitchApiController < ApplicationController
       grant_type: "client_credentials"
     }
 
-    res = HTTP[header].post(uri, json: body)
-    res_json = JSON.parse(res.to_s)
-    token = res_json["access_token"]
+    res = request_post(header, uri, body)
+    token = res["access_token"]
     render json: { "app_token": token }, status: :created
   end
+
+  # アプリアクセストークンを使用して、データ取得用アカウントのidを取得
+  def get_user
+    login_name = "rabbit_kun_2nd"
+    header = { "Authorization" => "Bearer " + ENV["APP_ACCESS_TOKEN"],  "Client-id" => ENV["CLIENT_ID"] }
+    uri = "https://api.twitch.tv/helix/users?login=#{login_name}"
+
+    res = request_get(header, uri)
+    user_id = res["data"][0]["id"]
+    render json: { "user_id": user_id }, status: :ok
+  end
+
+  private
+    def request_get(header, uri)
+      res = HTTP[header].get(uri)
+      JSON.parse(res.to_s)
+    end
+
+    def request_post(header, uri, body)
+      res = HTTP[header].get(uri, json: body)
+      JSON.parse(res.to_s)
+    end
 end
