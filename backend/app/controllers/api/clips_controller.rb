ddba020b2ad344d1642ef1c8c555d4613a980ba2
Taku1103 <orzorz1243@gmail.com>
2024-01-30 22:45:24 +0900

    :bug: clips_controllerのタイポ修正

--- backend/app/controllers/api/clips_controller.rb
+++ backend/app/controllers/api/clips_controller.rb
@@ -2,7 +2,7 @@ module Api
   class ClipsController < ApplicationController
     def show
       @clip = Clip.find(params[:id])
-      render json: { status: :ok, message:  "show action sucess! data WIP", clip: @clips }
+      render json: { status: :ok, message:  "show action sucess! data WIP", clip: @clip }
     end
   end
 end
