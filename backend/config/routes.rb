5010eba8e7e58989cbf5cfbb9b6ade5ddb14bdcb
takuya-38 <takuya.kogoma.38@gmail.com>
2024-01-31 20:59:23 +0900

    :sparkles: seeds.rb ver.1完成、ランキングクリ-Aidを取得するコントローラーを作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -12,4 +12,12 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  # twitch_api (使用しない)
+  get "/twitch_api/create_app_token", to: "twitch_api#create_app_token"
+  get "/twitch_api/create_user_token", to: "twitch_api#create_user_token"
+  get "/twitch_api/get_user_id", to: "twitch_api#get_user_id"
+  get "/twitch_api/get_user_channels", to: "twitch_api#get_user_channels"
+  get "/twitch_api/get_broadcaster_clips", to: "twitch_api#get_broadcaster_clips"
+  get "/ranking_playlists/get_ranking_clip_ids", to: "ranking_playlists#get_ranking_clip_ids"
 end
