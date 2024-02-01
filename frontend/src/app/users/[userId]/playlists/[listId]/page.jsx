5573d7bb1de6735950f123c6dde5929ae9f14be3
I635230 <yugo.susuki@gmail.com>
2024-02-01 13:38:56 +0900

    :+1: Xで共有ボタンにリンクを追加

--- frontend/src/app/users/[userId]/playlists/[listId]/page.jsx
+++ frontend/src/app/users/[userId]/playlists/[listId]/page.jsx
@@ -1,11 +1,15 @@
 import { fetchPlaylistData } from '@/app/lib/data'
 import Playlist from '@/app/ui/playlists/playlist/playlist'
 import PlaylistName from '@/app/ui/playlists/playlist/playlistName'
+import XShareButton from '@/app/ui/playlists/playlist/x-share-button'
 
 export default async function Page({ params }) {
+  const userId = params.userId
+  const listId = params.listId
+
   const listData = await fetchPlaylistData({
-    userId: params.userId,
-    listId: params.listId,
+    userId,
+    listId,
   })
 
   function displayDate(date) {
@@ -18,16 +22,16 @@ export default async function Page({ params }) {
     <>
       <div className="playlist-header">
         <p>公開プレイリスト</p>
-        <PlaylistName
-          listData={listData}
-          userId={params.userId}
-          listId={params.listId}
-        />
-        <p>作成者：{params.userId}</p>
+        <PlaylistName listData={listData} userId={userId} listId={listId} />
+        <p>作成者：{userId}</p>
         <p>作成日：{displayDate(listData.playlist.created_at)}</p>
         <p>お気に入り数：{listData.playlist.favorite_count}</p>
         <p>削除</p>
-        <p>Xで共有</p>
+        <XShareButton
+          userId={userId}
+          listId={listId}
+          listName={listData.playlist.name}
+        />
       </div>
       <Playlist listData={listData} />
     </>
