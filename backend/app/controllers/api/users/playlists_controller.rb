b8297371dc795f4c2b3e08fa5fc509de41777a34
Taku1103 <orzorz1243@gmail.com>
2024-02-01 22:53:51 +0900

    :+1: favoriteの数を取得 favorite_countのカラムとデータをマージする必要あり

--- backend/app/controllers/api/users/playlists_controller.rb
+++ backend/app/controllers/api/users/playlists_controller.rb
@@ -2,6 +2,7 @@ module Api
   module Users
     class PlaylistsController < ApplicationController
       before_action :set_playlist, only: [:show, :update, :destroy]
+      before_action :count_favorites, only: [:show]
 
       # user_idをパスパラメータで取得
       def index
@@ -12,7 +13,7 @@ module Api
 
       def show
         @playlist_clips = @playlist.clips
-        render json: { status: :ok, message: "showing success", playlist: @playlist, playlist_clips: @playlist_clips }
+        render json: { status: :ok, message: "showing success", playlist: @playlist, playlist_clips: @playlist_clips, favorite_count: @favorite_count }
       end
 
       def create
@@ -40,6 +41,10 @@ module Api
       private
         def set_playlist
           @playlist = Playlist.find(params[:id])
+        end
+
+        def count_favorites
+          @favorite_count = @playlist.user_favorite_playlists.count
         end
 
         def playlist_param
