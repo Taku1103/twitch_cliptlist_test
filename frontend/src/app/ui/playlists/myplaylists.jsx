d1c425944c3e6e16546db8187aaf3b217ccf001e
kajikarel <edgedesu@icloud.com>
2024-02-02 08:32:48 +0900

    :sparkles: タブで表示を切り替え

--- frontend/src/app/ui/playlists/myplaylists.jsx
+++ frontend/src/app/ui/playlists/myplaylists.jsx
@@ -1,8 +1,11 @@
 'use client'
 import PlaylistItem from '@/app/ui/playlists/playlistitem'
 import styles from '@/app/ui/playlists/playlists.module.css'
+import Favorite_list from '@/app/ui/sidevar/favorite_list'
+import { useState } from 'react'
 
 export default function Myplaylists({ userId, listsData }) {
+  const [activeTab, setActiveTab] = useState('playlists') // 初期タブをプレイリストに設定
   const chunkedArray = []
   const chunkSize = 3
   const displayRowMax = 3
@@ -12,21 +15,41 @@ export default function Myplaylists({ userId, listsData }) {
   }
 
   return (
-    <div className={styles.playlists}>
-      <div className={styles.spaceLeft}></div>
-      <div className={styles.column}>
-        {chunkedArray.map((column, index) => (
-          <div key={index}>
-            {column.map((listData) => (
-              <div className={styles.playlist} key={listData.id}>
-                <PlaylistItem listData={listData} />
+    <>
+      <div className="playlists-tab">
+        <button
+          className="tab-button"
+          onClick={() => setActiveTab('playlists')}
+        >
+          プレイリスト
+        </button>
+        <button
+          className="tab-button"
+          onClick={() => setActiveTab('favorites')}
+        >
+          お気に入り
+        </button>
+      </div>
+
+      {activeTab === 'playlists' && (
+        <div className={styles.playlists}>
+          <div className={styles.spaceLeft}></div>
+          <div className={styles.column}>
+            {chunkedArray.map((column, index) => (
+              <div key={index}>
+                {column.map((listData) => (
+                  <div className={styles.playlist} key={listData.id}>
+                    <PlaylistItem listData={listData} />
+                  </div>
+                ))}
+                <div className="clear-left"></div>
               </div>
             ))}
-            <div className="clear-left"></div>
           </div>
-        ))}
-      </div>
-      <div className={styles.spaceRight}></div>
-    </div>
+          <div className={styles.spaceRight}></div>
+        </div>
+      )}
+      {activeTab === 'favorites' && <Favorite_list />}
+    </>
   )
 }
--- frontend/src/app/users/[userId]/playlists/page.jsx
+++ frontend/src/app/users/[userId]/playlists/page.jsx
@@ -3,17 +3,13 @@ import Myplaylists from '@/app/ui/playlists/myplaylists'
 
 export default async function Page({ params }) {
   const listsData = await fetchPlaylists({ userId: params.userId })
-  // responseが正しく取得されたかを確認し、user_playlistsを取得
 
   return (
     <>
       <div className="playlists-name">
         <h1>Unameのプレイリスト一覧</h1>
       </div>
-      <div className="playlists-tab">
-        <button className="tab-button">プレイリスト</button>
-        <button className="tab-button">お気に入り</button>
-      </div>
+
       {/* propsでpramsのuserIdを渡す */}
       <Myplaylists userId={params.userId} listsData={listsData} />
     </>
