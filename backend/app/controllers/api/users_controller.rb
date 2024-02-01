916c84f2c96be99d7363ddd19f864ad25dc29adf
takuya-38 <takuya.kogoma.38@gmail.com>
2024-02-01 11:14:37 +0900

    :sparkles: users#createの作成

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,11 +1,42 @@
+require "http"
+
 module Api
   class UsersController < ApplicationController
+    before_action :extract_headers, only: [:show, :create]
+
     def show
       render status: :ok
     end
 
     def create
-      render status: :created
+      res = request_twitch_api(
+        header: { "Authorization" => @user_token,  "Client-id" => ENV["CLIENT_ID"] },
+        url: "https://api.twitch.tv/helix/users"
+      )
+
+      user = User.new(
+        id_on_twitch: res["id"].to_i,
+        login_name: res["login"],
+        display_name: res["display_name"],
+        profile_image_url: res["profile_image_url"],
+        user_created_at: res["created_at"]
+      )
+
+      if user.save
+        render json: user, status: :created
+      else
+        render json: user.errors, status: :unprocessable_entity
+      end
     end
+
+    private
+      def extract_headers
+        @user_token = request.headers["Authorization"]
+      end
+
+      def request_twitch_api(header:, url:)
+        res = HTTP[header].get(url)
+        JSON.parse(res.to_s)["data"][0]
+      end
   end
 end
