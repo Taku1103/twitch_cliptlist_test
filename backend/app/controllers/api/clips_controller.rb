ca5ab44d7f6fb6f266e1e6e1ed6f080092b90f7b
Taku1103 <orzorz1243@gmail.com>
2024-02-01 18:53:13 +0900

    :+1: clips_controllerにclip情報にbroadcaster_profile_image_urlを追加しました

--- backend/app/controllers/api/clips_controller.rb
+++ backend/app/controllers/api/clips_controller.rb
@@ -1,8 +1,8 @@
 module Api
   class ClipsController < ApplicationController
     def show
-      @clip = Clip.includes(:game).find(params[:id])
-      render json: { status: :ok, message: "show action sucess!",  clip: @clip.attributes.merge(game_title: @clip.game&.game_title) }
+      @clip = Clip.includes(:game).includes(:broadcaster).find(params[:id])
+      render json: { status: :ok, message: "show action success!",  clip: @clip.attributes.merge(game_title: @clip.game&.game_title, broadcaster_profile_image_url: @clip.broadcaster&.profile_image_url) }
     end
   end
 end
