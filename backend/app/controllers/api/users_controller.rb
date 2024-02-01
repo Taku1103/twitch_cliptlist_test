38e768b19fb074209704928d3e3bac910dfced57
takuya-38 <takuya.kogoma.38@gmail.com>
2024-02-01 17:50:52 +0900

    :recycle: users_controllers.rbのリファクタリング

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -6,58 +6,72 @@ module Api
     before_action :set_user, only: [:show]
 
     def show
-      res = request_twitch_api(
-        header: { "Authorization" => @user_token,  "Client-id" => ENV["CLIENT_ID"] },
-        url: "https://api.twitch.tv/helix/channels/followed?user_id=#{@user["id_on_twitch"]}&first=100"
-      )
-      followed_channels = res["data"]
-
-      # TODO: broadcasterテーブル作成後に確認(テーブル名、カラム名)
-      # channel_ids = followed_channels.map { |channel| channel["broadcaster_id"] }
-      # broadcaster_list = broadcaster.where(broadcaster_id: channel_ids)
-
-      followed_channels.length.times do |index|
-        # followed_channels[index]["profile_image_url"] = broadcaster_list[index]
-        followed_channels[index]["profile_image_url"] = "Hello"
+      res = request_twitch_api("https://api.twitch.tv/helix/channels/followed?user_id=#{@user.id_on_twitch}&first=100")
+      if res.status.success?
+        followed_channels = process_followed_channels(JSON.parse(res.body)["data"])
+        render json: { followed_channels: followed_channels }, status: :ok
+      else
+        render_api_error
       end
-
-      render json: { followed_channels: followed_channels }, status: :ok
     end
 
     def create
-      res = request_twitch_api(
-        header: { "Authorization" => @user_token,  "Client-id" => ENV["CLIENT_ID"] },
-        url: "https://api.twitch.tv/helix/users"
-      )
-      user_data = res["data"][0]
-
-      user = User.new(
-        id_on_twitch: user_data["id"].to_i,
-        login_name: user_data["login"],
-        display_name: user_data["display_name"],
-        profile_image_url: user_data["profile_image_url"],
-        user_created_at: user_data["created_at"]
-      )
-
-      if user.save
-        render json: user, status: :created
+      res = request_twitch_api("https://api.twitch.tv/helix/users")
+      if res.status.success?
+        user_data = JSON.parse(res.body)["data"][0]
+        user = build_user(user_data)
+        save_user(user)
       else
-        render json: user.errors, status: :unprocessable_entity
+        render_api_error
       end
     end
 
     private
+      def request_twitch_api(url)
+        header = { "Authorization" => @user_token, "Client-id" => ENV["CLIENT_ID"] }
+        HTTP[header].get(url)
+      end
+
+      # TODO: broadcasterテーブル完成後、実行確認。
+      def process_followed_channels(data)
+        # channel_ids = data.map { |channel| channel["broadcaster_id"] }
+        # broadcaster_list = Broadcaster.where(broadcaster_id: channel_ids)
+
+        data.each_with_index do |channel, index|
+          # channel["profile_image_url"] = broadcaster_list[index]
+          channel["profile_image_url"] = "Hello"
+        end
+        data
+      end
+
+      def build_user(user_data)
+        User.new(
+          id_on_twitch: user_data["id"].to_i,
+          login_name: user_data["login"],
+          display_name: user_data["display_name"],
+          profile_image_url: user_data["profile_image_url"],
+          user_created_at: user_data["created_at"]
+        )
+      end
+
+      def save_user(user)
+        if user.save
+          render json: user, status: :created
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def render_api_error
+        render json: { error: "API request failed" }, status: :bad_gateway
+      end
+
       def set_user
         @user = User.find(params[:id])
       end
 
       def extract_headers
         @user_token = request.headers["Authorization"]
-      end
-
-      def request_twitch_api(header:, url:)
-        res = HTTP[header].get(url)
-        JSON.parse(res.to_s)
       end
   end
 end
