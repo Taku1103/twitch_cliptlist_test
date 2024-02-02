9dbb7e4c8141ade6f86c67d020d035bec8b64fe4
Taku1103 <orzorz1243@gmail.com>
2024-02-03 02:31:18 +0900

    :bug: playlistが削除できない問題解決

--- backend/app/models/clip.rb
+++ backend/app/models/clip.rb
@@ -1,5 +1,5 @@
 class Clip < ApplicationRecord
-  has_many :playlist_clips
+  has_many :playlist_clips, dependent: :destroy
   has_many :playlists, through: :playlist_clips
   belongs_to :game, optional: true
   belongs_to :broadcaster, optional: true
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :user_favorite_playlists
-  has_many :playlists, through: :user_favorite_playlists
+  has_many :user_favorite_playlists, dependent: :destroy
+  has_many :playlists, through: :user_favorite_playlists, dependent: :destroy
 
   def favorite_playlist?(playlist)
     self.playlists.include?(playlist)
