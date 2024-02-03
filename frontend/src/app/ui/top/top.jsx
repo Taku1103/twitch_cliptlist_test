1d3ed47cc7c29379963cd2789ebc100c62ea01fd
Taku1103 <orzorz1243@gmail.com>
2024-02-03 23:03:38 +0900

    :up: Topのテキストと横線ともろもろ

--- frontend/src/app/ui/top/top.jsx
+++ frontend/src/app/ui/top/top.jsx
@@ -4,7 +4,6 @@ import Clips from '@/app/ui/top/clips/clips'
 import Playlists from '@/app/ui/top/playlists/playlists'
 import SortType from '@/app/ui/top/sortType'
 import styles from '@/app/ui/top/top.module.css'
-import Link from 'next/link'
 import { useRef, useState } from 'react'
 
 export default function Top({
@@ -17,10 +16,10 @@ export default function Top({
   const sortType = useRef('daily')
   return (
     <div className={styles.top}>
-      <h2 className={styles.h2}>人気のプレイリスト 一覧</h2>
+      <h2 className={styles.h2}>人気のプレイリスト</h2>
       <Playlists listsData={listsData} />
-
-      <h2 className={styles.h2}>人気のクリップ 一覧</h2>
+      <hr />
+      <h2 className={styles.h2}>人気のクリップ</h2>
       <SortType
         setClipsData={setClipsData}
         dailyClipsData={dailyClipsData}
@@ -33,19 +32,6 @@ export default function Top({
         clipsData={clipsData.playlist_clips}
         listId={clipsData.playlist.id}
       />
-
-      <h2>確認用リンク(後で削除)</h2>
-      <ul>
-        <li>
-          <Link href="/users/2/playlists/4">suuusanのプレイリスト1つ目</Link>
-        </li>
-        <li>
-          <Link href="/watch/?clip=1">Example Clip</Link>
-        </li>
-        <li>
-          <Link href="/watch/?clip=46&list=4">Example Clip(listData付き)</Link>
-        </li>
-      </ul>
     </div>
   )
 }
