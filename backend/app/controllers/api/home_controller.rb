9434de10af8b566758260541d1c622ae9bc95511
takuya-38 <takuya.kogoma.38@gmail.com>
2024-02-04 15:08:08 +0900

    :sparkles: Homeコントローラーの作成者・作成者サムネイルの追加

--- backend/app/controllers/api/home_controller.rb
+++ backend/app/controllers/api/home_controller.rb
@@ -1,62 +1,43 @@
 module Api
   class HomeController < ApplicationController
     def index
-      @playlists = fetch_playlists
-      @playlist_ids = @playlists.pluck(:id)
-      add_favorite_count
-      sort_favorite_count
+      fetch_playlists
 
-      clip_count_hash = fetch_clip_count_hash
-      playlist_clip_ids = PlaylistClip.where(playlist_id: @playlist_ids)
-                                      .order(order_index: :asc)
-                                      .pluck(:playlist_id, :clip_id)
-      clip_id_hash = playlist_clip_ids.uniq { |playlist_id, clip_id| playlist_id }.to_h
+      @playlist_data = @playlists.map do |playlist|
+        first_clip_id = playlist.clips[0].id
+        first_thumbnail_url = playlist.clips[0].thumbnail_url
 
-      thumbnail_url_hash = Clip.find(clip_id_hash.values)
-                          .pluck(:id, :thumbnail_url).to_h
+        clip_count = count_clips(playlist)
+        favorite_count = count_favorites(playlist)
 
-      @playlists.each do |playlist|
-        playlist["first_clip_id"] = clip_id_hash[playlist["id"]]
-        playlist["first_thumbnail_url"] = thumbnail_url_hash[clip_id_hash[playlist["id"]]]
-        playlist["clip_count"] = clip_count_hash[playlist["id"]]
+
+        playlist_creator_name = playlist.user.display_name
+        creator_image_url = playlist.user.profile_image_url
+
+        playlist.attributes.merge({
+          first_clip_id: first_clip_id,
+          first_thumbnail_url: first_thumbnail_url,
+          clip_count: clip_count,
+          favorite_count: favorite_count,
+          playlist_creator_name: playlist_creator_name,
+          playlist_creator_profile_image_url: creator_image_url
+        })
       end
 
-      render json: { playlists: @playlists }, status: :ok
+      sort_favorite_count
+      render json: { playlists: @playlist_data }, status: :ok
     end
 
     private
       def fetch_playlists
-        Playlist.where(created_at: 7.days.ago.beginning_of_day..Time.now)
-                .where.not("name like ?", "%ranking")
-      end
-
-      def fetch_clip_count_hash
-        PlaylistClip.where(playlist_id: @playlist_ids)
-                    .group(:playlist_id)
-                    .count
-      end
-
-      def add_favorite_count
-        @playlists = @playlists.as_json
-        favorite_count_list = UserFavoritePlaylist.where(playlist_id: @playlist_ids).group(:playlist_id).count
-        @playlists.each do |playlist|
-          favorite_count = favorite_count_list[playlist["id"]] || 0
-          playlist["favorite_count"] = favorite_count
-        end
+        @playlists = Playlist.includes(:user).includes(:clips)
+                              .where(created_at: 7.days.ago.beginning_of_day..Time.now)
+                              .where.not("name like ?", "%ranking")
       end
 
       def sort_favorite_count
-        sorted_playlists = @playlists.sort_by { |playlist| -playlist["favorite_count"] }
-        @playlists = sorted_playlists
-      end
-
-      def fetch_thumbnail_url_hash
-        clip_ids = PlaylistClip.where(playlist_id: @playlist_ids)
-                              .order(order_index: :asc)
-                              .pluck(:clip_id)
-                              .uniq
-
-        Clip.where(id: clip_ids).pluck(:id, :thumbnail_url).to_h
+        sorted_playlists = @playlist_data.as_json.sort_by { |playlist| -playlist["favorite_count"] }
+        @playlist_data = sorted_playlists
       end
   end
 end
